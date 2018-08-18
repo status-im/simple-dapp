98fd39f768885a07f68b74ad71841a449f9e745d
Goran Jovic <goranjovic@gmail.com>
2018-08-18 11:18:47 +0200

    fixed gas limit param

--- src/cljs/status_dapp/components.cljs
+++ src/cljs/status_dapp/components.cljs
@@ -32,6 +32,6 @@
 
 (defn asset-button [label asset-address]
   [react/view {:style {:margin-bottom 10}}
-   [button (str "Request " label) #(re-frame/dispatch [:send-transaction {:to       asset-address
-                                                                          :value    0
-                                                                          :gasPrise 150000}])]])
+   [button (str "Request " label) #(re-frame/dispatch [:send-transaction {:to    asset-address
+                                                                          :value 0
+                                                                          :gas   150000}])]])
