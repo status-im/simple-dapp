2ec91cb78934427ff5d375d0aa5cedab785ec81e
Andrey Shovkoplyas <motor4ik@gmail.com>
2019-01-11 13:11:11 +0100

    don't use beta provider for now

--- src/cljs/status_dapp/db.cljs
+++ src/cljs/status_dapp/db.cljs
@@ -2,13 +2,13 @@
   (:require [re-frame.core :as re-frame]))
 
 (def web3 (or (when (exists? js/web3) js/web3)
-              (when (exists? js/ethereumBeta)
-                (js/setTimeout (fn []
-                                 (.then (.send js/ethereumBeta "eth_requestAccounts") #(re-frame/dispatch [:set-default-account]))
-                                 (when js/ethereumBeta
-                                   (.then (.getContactCode js/ethereumBeta.status) #(re-frame/dispatch [:on-status-api :contact %]))))
-                               100)
-                (js/Web3. js/ethereumBeta))
+              #_(when (exists? js/ethereumBeta)
+                  (js/setTimeout (fn []
+                                   (.then (.send js/ethereumBeta "eth_requestAccounts") #(re-frame/dispatch [:set-default-account]))
+                                   (when js/ethereumBeta
+                                     (.then (.getContactCode js/ethereumBeta.status) #(re-frame/dispatch [:on-status-api :contact %]))))
+                                 100)
+                  (js/Web3. js/ethereumBeta))
               (when (exists? js/ethereum)
                 (js/setTimeout (fn []
                                  (.then (.enable js/ethereum) #(re-frame/dispatch [:set-default-account]))
